078880259faf4a451d2138b8eb800855de242335
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-01-06 14:09:18 +0700

    Update index.ts

--- packages/supabase/src/index.ts
+++ packages/supabase/src/index.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 import { env } from "./env";
 
-export const supabaseClient = createBrowserClient(
+export const supabaseClient: any = createBrowserClient(
   env.NEXT_PUBLIC_SUPABASE_URL,
   env.NEXT_PUBLIC_SUPABASE_KEY
 );
